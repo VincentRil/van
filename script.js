0edac4b7b234665a1e55b24c5789a02501fe2fd3
Vincent <pingszz47@gmail.com>
2026-07-27 22:48:11 +0800

    hapus komen script

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// =============================
-// SUPABASE
-// =============================
-
 const SUPABASE_URL = "https://jmbtdfjryeuoabhmecoj.supabase.co";
 
 const SUPABASE_KEY = "sb_publishable_z2mPCm9C9t29x8pKr_gmCw_a_CPyPOG";
